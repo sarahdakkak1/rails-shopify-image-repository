d7b78e3ad46c0ba3851fce7c9e74ef85a43fdeff
sarah dakkak <sarah.dakkak1@gmail.com>
2021-01-12 00:34:06 -0500

    fixed bugg

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    root to: 'stories#home'
+    root to: 'stories#index'
     resources :stories, only: [:index, :show, :new, :create, :destroy]
 end
